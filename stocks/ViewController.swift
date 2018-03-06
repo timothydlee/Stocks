c9cdd39df5cd08a138b20d4196abf2cbb67b345a
Tim Lee <timothy.d.lee@live.com>
2018-03-05 23:19:20 -0500

    Got call to Alpha Vantage API to work

--- stocks/ViewController.swift
+++ stocks/ViewController.swift
@@ -7,11 +7,27 @@
 //
 
 import UIKit
+import Alamofire
+
+let STOCKS_URL = ""
 
 class ViewController: UIViewController {
+    
+    let STOCKS_URL = "https://www.alphavantage.co/query?"
+    let APP_ID = "YF4GKFKVSW54BMH4"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Alamofire.request("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=INTL&interval=60min&apikey=\(APP_ID)").responseJSON {
+            response in
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            } else {
+                print("Error \(String(describing: response.result.error))")
+            }
+            
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
