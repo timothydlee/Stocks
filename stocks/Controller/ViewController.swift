7d393eb11bd1e05873c964ca790e712f17883f33
Tim Lee <timothy.d.lee@live.com>
2018-03-08 23:11:18 -0500

    Trying to get the stocksArray to be visible to different functions

--- stocks/Controller/ViewController.swift
+++ stocks/Controller/ViewController.swift
@@ -10,59 +10,86 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let STOCKS_URL = "https://www.alphavantage.co/query"
-        let SYMBOL = "INTL"
-        let INTERVAL = "60min"
-        let APP_ID = "YF4GKFKVSW54BMH4"
-        let initialStockParams : [String : String] = ["function" : "TIME_SERIES_DAILY", "symbol" : SYMBOL, "interval" : INTERVAL, "apikey" : APP_ID]
-        let batchStockParams : [String : String] = ["function" : "BATCH_STOCK_QUOTES", "symbols" : "MSFT,AAPL,INTL", "apikey" : APP_ID]
-
-//        getStocksData(url: STOCKS_URL, parameters: initialStockParams)
-        getStocksData(url: STOCKS_URL, parameters: batchStockParams)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         
+        let STOCKS_URL = "https://www.alphavantage.co/query"
+        let APP_ID = "YF4GKFKVSW54BMH4"
+        let batchStockParams : [String : String] = ["function" : "BATCH_STOCK_QUOTES", "symbols" : "MSFT,AAPL,INTL", "apikey" : APP_ID]
+        let res = getStocksData(url: STOCKS_URL, parameters: batchStockParams)
+        print(res)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "stockCell")
+        
+        return cell!
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        let STOCKS_URL = "https://www.alphavantage.co/query"
+//        let APP_ID = "YF4GKFKVSW54BMH4"
+//        let batchStockParams : [String : String] = ["function" : "BATCH_STOCK_QUOTES", "symbols" : "MSFT,AAPL,INTL", "apikey" : APP_ID]
+//
+//        let result = getStocksData(url: STOCKS_URL, parameters: batchStockParams)
+//
+//        print(result)
+        return 1
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    
-    @IBOutlet weak var stockLabel: UILabel!
     
     //MARK: Get Initial Stock Call
     /***************************************************************/
 
-    func getStocksData(url: String, parameters: [String : String]) {
+    func getStocksData(url: String, parameters: [String : String])  {
         Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
             response in
+            
             if response.result.isSuccess {
                 
-                let stockJSON : JSON = JSON(response.result.value!)
-                self.updateStockData(json: stockJSON)
+                let result = response.result.value!
+                let stockJSON : JSON = JSON(result)
+                let stocksArray = self.updateStockData(json: stockJSON)
+                print(stocksArray)
                 
             } else {
                 
                 print("Failed")
                 
             }
+            
         }
     }
     
     //MARK: Updates Stocks and Parses JSON
-    func updateStockData(json: JSON) {
-        
+    /***************************************************************/
+    
+    func updateStockData(json: JSON) -> Array<Any> {
+
+        var stocksArray = [] as Array
+
         for stock in 0..<json["Stock Quotes"].count {
-            print(json["Stock Quotes"][stock])
+            stocksArray.append(json["Stock Quotes"][stock])
         }
         
+
+        return stocksArray
     }
+    
+    
 
 }
 
