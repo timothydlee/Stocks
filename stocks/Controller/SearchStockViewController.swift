7df18ced11e0dbd9bd2df91df47848158173ea6d
Tim Lee <timothy.d.lee@live.com>
2018-03-13 19:55:07 -0400

    Adds functionality of search button going to Alphavantage API and finding TIME_SERIES_DAILY info on the stock that user inputs.

--- stocks/Controller/SearchStockViewController.swift
+++ stocks/Controller/SearchStockViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import Alamofire
 
 class SearchStockViewController: UIViewController {
+    
+    
+    let STOCKS_URL = "https://www.alphavantage.co/query"
+    let APP_ID = "YF4GKFKVSW54BMH4"
     
     //IBOutlet defining TextField that user inputs stock name
     @IBOutlet weak var searchStockTextField: UITextField!
@@ -19,20 +24,37 @@ class SearchStockViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    //IBAction that initiates search for stock that user inputs
     @IBAction func searchForStockButtonPressed(_ sender: Any) {
         if let searchInput = searchStockTextField.text {
             print(searchInput)
+            let params : [String : String] = ["function" : "TIME_SERIES_DAILY", "symbol" : searchInput, "apikey" : "YF4GKFKVSW54BMH4"]
+            self.checkStockFromInput(url: STOCKS_URL, parameters: params)
         } else {
             return
         }
     }
     
-    
+    //MARK: checkStockFromInput is function that takes user input and makes call to Alphavantage API
+    func checkStockFromInput(url: String, parameters: [String : String]) {
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            
+            if response.result.isSuccess {
+                
+                let result = response.result.value!
+                print(result)
+                
+            } else {
+                
+                print("Error \(String(describing: response.result.error))")
+            }
+        }
+    }
 }
