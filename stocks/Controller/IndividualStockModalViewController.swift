9201c0c51814107718da78f3c59171a645385a57
Tim Lee <timothy.d.lee@live.com>
2018-03-13 21:26:48 -0400

    Gave popup rounded edges.

--- stocks/Controller/IndividualStockModalViewController.swift
+++ stocks/Controller/IndividualStockModalViewController.swift
@@ -10,13 +10,19 @@ import UIKit
 
 class IndividualStockModalViewController: UIViewController {
 
-
+    //Creates IBOutlet reference to the Modal view itself
+    @IBOutlet weak var stockModal: UIView!
+    
+    //IBAction that defines how to dismiss the popup once user is done
     @IBAction func closeButtonPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Gives popup rounded corners.
+        stockModal.layer.cornerRadius = 8
     }
     
 }
