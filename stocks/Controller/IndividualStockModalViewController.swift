af182810c3da692a5278ce101c52421833f6b02a
Tim Lee <timothy.d.lee@live.com>
2018-03-13 20:59:43 -0400

    Added modal to the storyboard and also created Close Button functionality with the modal.

---
+++ stocks/Controller/IndividualStockModalViewController.swift
@@ -0,0 +1,21 @@
+//
+//  IndividualStockModalViewController.swift
+//  stocks
+//
+//  Created by Timothy Lee on 3/13/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class IndividualStockModalViewController: UIViewController {
+
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
